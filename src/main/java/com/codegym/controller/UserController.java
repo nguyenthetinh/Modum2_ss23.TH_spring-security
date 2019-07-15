37de76985d6f90b6c2e8e7edbd0cccf700876222
nguyenthetinh <tinhnt0207@gmail.com>
2019-07-15 16:05:40 +0700

    bai

---
+++ src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.codegym.controller;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user(Principal principal){
+        // get authenticated user name form principal
+        System.out.println(principal.getName());
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        //get authenticated user name from securityContext
+        SecurityContext context= SecurityContextHolder.getContext();
+        System.out.println(context.getAuthentication().getName());
+        return "admin";
+    }
+}
